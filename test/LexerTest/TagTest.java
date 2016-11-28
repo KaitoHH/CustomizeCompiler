8b40871138537d74ca7942dcb13fd25438c0b8d4
CtheSky <cthesky@yeah.net>
2016-11-28 23:40:18 +0800

    test：add test for Lexer.Tag

---
+++ test/LexerTest/TagTest.java
@@ -0,0 +1,48 @@
+package LexerTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import Lexer.Tag;
+
+/**
+ * Project: CustomizeCompiler
+ * Author: CtheSky
+ * Create Date: 2016/11/28
+ * Description:
+ * All rights reserved.
+ */
+public class TagTest {
+
+    @Test
+    public void initializeTest(){
+        assertNotNull(Tag.KEY);
+        assertTrue(Tag.KEY instanceof Map);
+        assertTrue(Tag.KEY.isEmpty());
+    }
+
+    @Test
+    public void addKeyTest(){
+        Tag.KEY.clear();
+
+        Set<Integer> checkDulicate = new HashSet<>();
+        for (int i = 1; i <= 100; i++) {
+            String key = "" + i;
+
+            // check whether key is added
+            Tag.addKey(key);
+            assertEquals(Tag.KEY.size(), i);
+
+            // check whether id generated is unique
+            int id = Tag.KEY.get(key);
+            assertTrue(!checkDulicate.contains(id));
+            checkDulicate.add(id);
+        }
+
+        Tag.KEY.clear();
+    }
+}
