2e283cc30f1cc76a3b9067b331c5c962c31e0346
CtheSky <cthesky@yeah.net>
2016-12-01 17:28:00 +0800

    feat: match single automata

---
+++ src/Lexer/Automata/AutomataRunner.java
@@ -0,0 +1,53 @@
+package Lexer.Automata;
+
+import Lexer.Token.Tag;
+import Lexer.Token.Token;
+import Lexer.Token.Word;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Project: CustomizeCompiler
+ * Author: CtheSky
+ * Create Date: 2016/12/1
+ * Description:
+ * All rights reserved.
+ */
+public class AutomataRunner {
+
+    public static List<Token> run(List<Automata> automataList, String input) {
+        //TODO: complete this function
+        return null;
+    }
+
+    public static Word matchSingleAutomata(Automata automata, String input, int start) {
+        if (automata == null || input == null || start >= input.length()) return null;
+
+        AutomataNode currentNode = automata.getInitialNode();
+        Set<AutomataEdge> edges;
+        int matchPosition = -1;
+        char nextChar;
+
+        for (int currentPosition = start; currentNode != null && currentPosition < input.length(); currentPosition++) {
+            nextChar = input.charAt(currentPosition);
+            edges = currentNode.getEdgeSet();
+            currentNode = null;
+
+            for (AutomataEdge edge: edges) {
+                if (edge.getCondition().contains(nextChar)) {
+                    currentNode = edge.getDest();
+                    break;
+                }
+            }
+
+            if (automata.isAccept(currentNode))
+                matchPosition = currentPosition + 1;
+        }
+
+        if (matchPosition == -1)
+            return null;
+        else
+            return new Word(Tag.ID, input.substring(start, matchPosition));
+    }
+}
