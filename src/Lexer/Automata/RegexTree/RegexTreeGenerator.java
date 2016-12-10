9693c34ea03cb7636864b34e8e2203d63acd6a98
Hui,Huang <hh.kaito@gmail.com>
2016-12-10 18:59:08 +0800

    FEAT: generate regex tree

--- src/Lexer/Automata/RegexTree/RegexTreeGenerator.java
+++ src/Lexer/Automata/RegexTree/RegexTreeGenerator.java
@@ -1,5 +1,6 @@
 package Lexer.Automata.RegexTree;
 
+import java.util.Stack;
 import java.util.Vector;
 
 /**
@@ -14,9 +15,51 @@ public class RegexTreeGenerator {
 	private Vector<Character> suffixExp;
 
 	public RegexTreeGenerator(String regex) {
+		// 取得后缀表达式
 		PolishExp polishExp = new PolishExp(regex);
 		suffixExp = polishExp.getExp();
+
+		// 生成语法树
+		generateTree();
 	}
 
+	public void generateTree() {
+		RegexTree left = null, right = null;
+		Stack<RegexTree> stack = new Stack();
+		int cnt = 0;
+		for (char ch : suffixExp) {
+			switch (ch) {
+				case '+':
+					right = stack.pop();
+					left = stack.pop();
+					stack.push(new RegexTree(left, right, RegexTree.CAT));
+					break;
+				case '|':
+					right = stack.pop();
+					left = stack.pop();
+					stack.push(new RegexTree(left, right, RegexTree.OR));
+					break;
+				case '*':
+					stack.push(new RegexTree(stack.pop(), RegexTree.STAR));
+					break;
+				case '#':
+					RegexTree node = new RegexTree('#');
+					node.setNo(++cnt);
+					stack.push(new RegexTree(stack.pop(), node, RegexTree.CAT));
+					break;
+				default:
+					node = new RegexTree(ch);
+					node.setNo(++cnt);
+					stack.push(node);
+			}
+		}
+		if (stack.size() != 1) {
+			throw new StackOverflowError("stack size != 1");
+		}
+		root = stack.pop();
+	}
 
+	public static void main(String args[]) {
+		new RegexTreeGenerator("(a|b)*abb");
+	}
 }
