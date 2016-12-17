a066f7883f5a25c58879770368c09a10ae985135
CtheSky <cthesky@yeah.net>
2016-12-17 14:24:24 +0800

    feat: matchLongestToken using multi-threading

--- src/Lexer/Automata/AutomataRunner.java
+++ src/Lexer/Automata/AutomataRunner.java
@@ -4,6 +4,7 @@ import Lexer.Token.Tag;
 import Lexer.Token.Token;
 import Lexer.Token.Word;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -15,12 +16,57 @@ import java.util.Map;
  * All rights reserved.
  */
 public class AutomataRunner {
+    private static Token longest;
 
     public static List<Token> run(List<Automata> automataList, String input) {
         //TODO: complete this function
         return null;
     }
 
+    /** Find the longest matched token with automatas using multi-threading
+     *
+     * @param automatas	    Automatas to match with
+     * @param input	        String of input file
+     * @param start	        int of start position to match
+     * @return			        Longest matched Token
+     */
+    public static Token matchLongestToken(List<Automata> automatas, String input, int start) {
+        List<Thread> threads = new ArrayList<>();
+        longest = null;
+
+        for (Automata automata : automatas) {
+            Thread thread = new Thread(){
+                @Override
+                public void run() {
+                    Token matched = matchSingleAutomata(automata, input , start);
+                    if (matched != null)
+                        synchronized ("multi automata match lock") {
+                            if (longest == null || matched.getLength() > longest.getLength())
+                                longest = matched;
+                        }
+                }
+            };
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread t : threads)
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        return longest;
+    }
+
+    /** Find the longest matched token with a single automata
+     *
+     * @param automata	        Automata to match with
+     * @param input	        String of input file
+     * @param start	        int of start position to match
+     * @return			        Longest matched Token (currently return a Word)
+     */
     public static Word matchSingleAutomata(Automata automata, String input, int start) {
         if (automata == null || input == null || start >= input.length()) return null;
 
