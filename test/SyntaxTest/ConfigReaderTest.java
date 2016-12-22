6bc5a29599c9dccee5cb7cedf59c42e2ecd5328d
KaitoHH <hh.kaito@gmail.com>
2016-12-22 21:56:47 +0800

    test: add config reader test

---
+++ test/SyntaxTest/ConfigReaderTest.java
@@ -0,0 +1,21 @@
+package SyntaxTest;
+
+import Syntax.CFL;
+import Syntax.SyntaxConfig;
+import org.junit.Test;
+
+/**
+ * Project: CustomizeCompiler
+ * Author: KaitoHH
+ * Create Date: 2016/12/22
+ * Description:
+ * All rights reserved.
+ */
+public class ConfigReaderTest {
+	@Test
+	public void sampleTest() {
+		SyntaxConfig config = new SyntaxConfig();
+		CFL cfl = config.getCfl();
+		System.out.println(cfl.toString());
+	}
+}
