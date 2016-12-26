4843ef151a659140313b6ed9d54fd0173a71b16d
CtheSky <cthesky@yeah.net>
2016-12-26 14:18:12 +0800

    feat: UnDeclare statement

---
+++ src/Syntax/AST/Statements/UnDeclare.java
@@ -0,0 +1,21 @@
+package Syntax.AST.Statements;
+
+import Syntax.AST.Basic.Id;
+import Syntax.AST.Env;
+
+/**
+ * Project: CustomizeCompiler
+ * Author: CtheSky
+ * Create Date: 2016/12/26
+ * Description:
+ * All rights reserved.
+ */
+public class UnDeclare extends Stmt {
+    public final Id id;
+    public UnDeclare(Id id) { this.id = id; }
+
+    @Override
+    public void execute(Env env) {
+
+    }
+}
